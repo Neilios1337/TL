29fb4c3951861eb2dcd70bd5f2180fd4d9fb443d
Neilios1337 <neil.delamere@googlemail.com>
2020-05-01 18:07:40 +0100

    Update challenge.sh

--- challenge.sh
+++ challenge.sh
@@ -11,9 +11,9 @@ echo "$(ls)" >> ~/TrueLayer/TLTest.txt
 echo >> ~/TrueLayer/TLTest.txt
 echo "Public IP Address: $(dig +short myip.opendns.com @resolver1.opendns.com)" >> ~/TrueLayer/TLTest.txt
 echo >> ~/TrueLayer/TLTest.txt
-echo "Available RAM: $(sysctl -n hw.memsize)" >> ~/TrueLayer/TLTest.txt
+echo "Available RAM: $(free -m | gawk '/Mem:/{print $2}')MB" >> ~/TrueLayer/TLTest.txt #or "sysctl -n hw.memsize" for mac?
 echo >> ~/TrueLayer/TLTest.txt
-echo "PID: ($$) - Username: $(id -un)" >> ~/TrueLayer/TLTest.txt #ps -ef may also work for getting PID of running shell
+echo "PID: ($$) - Username: $(id -un)" >> ~/TrueLayer/TLTest.txt #"ps -ef" may also work for getting PID of running shell
 echo >> ~/TrueLayer/TLTest.txt
 echo "End of script" >> ~/TrueLayer/TLTest.txt
 echo "- - - - - - - - - - - - - -" >> ~/TrueLayer/TLTest.txt
